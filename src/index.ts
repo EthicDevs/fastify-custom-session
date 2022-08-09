cf50fc9fd4bbd1c15b77318bd419fc69646f2aeb
William Nemencha <40295432+WNemencha@users.noreply.github.com>
2022-08-09 16:36:51 +0200

    feat(adapters): add FirebaseSessionAdapter

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,14 @@ declare module "fastify" {
 
 import { makePlugin } from "./pluginFactory";
 
+export type {
+  Session,
+  CustomSession,
+  ISessionStoreAdapter,
+  ISessionStoreAdapterConstructable,
+} from "../src/types";
+
+export { FirebaseSessionAdapter } from "./adapters/firebase";
+// export { PostgresSessionAdapter } from "./adapters/postgres";
+
 export default makePlugin();
