e5a0ac5874879ba90d068f2558fb2d8652f880e7
William Nemencha <40295432+WNemencha@users.noreply.github.com>
2022-08-11 09:14:15 +0200

    fix(prisma_adapter): make sure findUnique return type is correct

--- src/adapters/prisma.ts
+++ src/adapters/prisma.ts
@@ -10,7 +10,7 @@ interface ISession {
   createdAt: Date;
   updatedAt: Date;
   expiresAt?: Date | null;
-  data?: {};
+  data?: {} | null;
   detectedUserAgent: string;
   detectedIPAddress: string;
 }
